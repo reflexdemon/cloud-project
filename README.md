9d409c35a83ccfbcbd2ab8c7ba13db44ec478041
reflexdemon <venkateswara.vp@gmail.com>
2018-02-20 19:30:14 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# cloud-project
+
+
+```bash
+$ curl https://start.spring.io/starter.tgz -d dependencies=web \
+-d language=java -d type=gradle-project -d baseDir=cloud-project | tar -xzvf -
+```
---
+++ src/main/java/com/example/demo/Greetings.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+public class Greetings {
+    private final long id;
+    private final String content;
+    public Greetings(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+    public long getId() {
+        return id;
+    }
+    public String getContent() {
+        return content;
+    }
+}
